2962e30faf925e8e847e2d5972d3d746d3d731af
Travier Moorlag <moorlagt@gmail.com>
2025-11-07 21:41:18 -0500

    added dasiyui, working on layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { FolderGit, GitPullRequest, Settings } from '@lucide/svelte';
 
 	let { children } = $props();
 </script>
@@ -9,4 +10,44 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="drawer min-h-screen lg:drawer-open">
+	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+
+	<!-- Main content -->
+	<div class="drawer-content flex flex-col">
+		<!-- Navbar/Toggle for mobile -->
+		<div class="navbar bg-base-300 lg:hidden">
+			<label for="my-drawer-3" class="btn btn-square btn-ghost">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block h-6 w-6 stroke-current"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					></path>
+				</svg>
+			</label>
+		</div>
+
+		<!-- Page content -->
+		<div class="flex-1 p-4">
+			{@render children()}
+		</div>
+	</div>
+
+	<!-- Sidebar -->
+	<div class="drawer-side">
+		<label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu min-h-full w-50 bg-base-200 p-4">
+			<!-- Sidebar content here -->
+			<li><a><FolderGit class="icon-sm" /> Repositories</a></li>
+			<li><a><GitPullRequest class="icon-sm" />Pull Requests</a></li>
+			<li><a><Settings class="icon-sm" /> Settings</a></li>
+		</ul>
+	</div>
+</div>
